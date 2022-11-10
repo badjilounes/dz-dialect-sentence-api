760ae41d2ca57b0c0000632f6989e6eb5f619e9c
Ghilès <ghiles.badji@gmail.com>
2022-11-10 11:04:58 +0100

    feat create sentence response dto

---
+++ .history/src/sentence/dto/sentence-response.dto_20221110105922.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class SentenceResponseDto {
+    @ApiProperty()
+    dz: string;
+
+    @ApiProperty()
+    dz_ar: string;
+
+    @ApiProperty()
+    fr: string;
+
+    @ApiProperty()
+    word_propositions_dz: string[];
+
+    @ApiProperty()
+    word_propositions_fr: string[];
+
+    @ApiProperty()
+    pronouns: string[];
+
+    @ApiProperty()
+    adjectives: string[];
+
+    @ApiProperty()
+    verbs: string[];
+
+    @ApiProperty()
+    tense: string;
+}
